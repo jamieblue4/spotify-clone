9ec261eddb508331f9a7917fc3e57e7ef7787859
Jamie Gist <jamie.gist.7@gmail.com>
2022-10-03 21:25:10 -0700

    Add button functionality. Add search feature and pull from API.

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useEffect, useState } from "react";
+import axios from 'axios';
 
 function App() {
 
@@ -9,22 +10,37 @@ function App() {
   const response_type = "token";
 
   const [token, setToken] = useState("")
+  const [searchKey, setSearchKey] = useState("")
 
   useEffect(() => {
     const hash = window.location.hash
-    let token = window.localStorage.getItem("token")
+    let token = window.localStorage.getItem('token')
 
     if(!token && hash) {
       token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
 
       window.location.hash = ""
-      window.localStorage.setItem("token", token)
+      window.localStorage.setItem('token', token)
       setToken(token)
     }
   }, [])
 
   const logout = () => {
-    window.localStorage.removeItem("token")
+    setToken("")
+    window.localStorage.removeItem('token');
+  }
+
+  const searchArtists = async (e) => {
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+        params: {
+          q: searchKey,
+          type: "artist"
+        }
+    }
+    )
   }
 
   return (
@@ -33,16 +49,26 @@ function App() {
         <p>
           Welcome to Spotify.
         </p>
-        { token ?
+        {!token ?
         <a
           className="login-link"
           href={`${auth_endpoint}?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}`}>
           Log In
         </a>
         : <button onClick={logout}>Log Out</button>}
+
+        {token ?
+        <form onSubmit={searchArtists}>
+          <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+          <button type={"submit"}>Search</button>
+        </form>
+
+        : <h2>Please Log In</h2>
+        }
+
       </header>
     </div>
   );
-    }
+  }
 
 export default App;
